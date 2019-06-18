4cf7c760eca51084c310e0d5508ee1e41cd15293
Mouad El Merchichi <melmerchichis2m@onestcom.com>
2019-06-18 11:53:21 +0100

    formatting code

--- backend/src/main/java/ma/hiddenfounders/codingchallenge/security/dto/AuthenticationRequest.java
+++ backend/src/main/java/ma/hiddenfounders/codingchallenge/security/dto/AuthenticationRequest.java
@@ -4,68 +4,75 @@ import java.io.Serializable;
 
 public class AuthenticationRequest implements Serializable {
 
-   private static final long serialVersionUID = -4246677518475722484L;
+	private static final long serialVersionUID = -4246677518475722484L;
 
-   private String email;
-   private String password;
+	private String email;
+	private String password;
 
-   public AuthenticationRequest() {
-   }
+	public AuthenticationRequest() {
+	}
 
-   public AuthenticationRequest(String email, String password) {
-      setEmail(email);
-      setPassword(password);
-   }
+	public AuthenticationRequest(String email, String password) {
+		setEmail(email);
+		setPassword(password);
+	}
 
-   public String getEmail() {
-      return this.email;
-   }
+	public String getEmail() {
+		return this.email;
+	}
 
-   public void setEmail(String email) {
-      this.email = email;
-   }
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-   public String getPassword() {
-      return this.password;
-   }
+	public String getPassword() {
+		return this.password;
+	}
 
-   public void setPassword(String password) {
-      this.password = password;
-   }
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
-   @Override
-   public int hashCode() {
-      final int prime = 31;
-      int result = 1;
-      result = prime * result + ((email == null) ? 0 : email.hashCode());
-      result = prime * result + ((password == null) ? 0 : password.hashCode());
-      return result;
-   }
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		return result;
+	}
 
-   @Override
-   public boolean equals(Object obj) {
-      if (this == obj)
-         return true;
-      if (obj == null)
-         return false;
-      if (getClass() != obj.getClass())
-         return false;
-      AuthenticationRequest other = (AuthenticationRequest) obj;
-      if (email == null) {
-         if (other.email != null)
-            return false;
-      } else if (!email.equals(other.email))
-         return false;
-      if (password == null) {
-         if (other.password != null)
-            return false;
-      } else if (!password.equals(other.password))
-         return false;
-      return true;
-   }
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		AuthenticationRequest other = (AuthenticationRequest) obj;
+		if (email == null) {
+			if (other.email != null) {
+				return false;
+			}
+		} else if (!email.equals(other.email)) {
+			return false;
+		}
+		if (password == null) {
+			if (other.password != null) {
+				return false;
+			}
+		} else if (!password.equals(other.password)) {
+			return false;
+		}
+		return true;
+	}
 
-   @Override
-   public String toString() {
-      return "AuthenticationRequest [email=" + email + ", password=" + password + "]";
-   }
+	@Override
+	public String toString() {
+		return String.format("AuthenticationRequest [email=%s, password=%s]", email, password);
+	}
 }
--- backend/src/main/java/ma/hiddenfounders/codingchallenge/security/handler/RestAccessDeniedHandler.java
+++ backend/src/main/java/ma/hiddenfounders/codingchallenge/security/handler/RestAccessDeniedHandler.java
@@ -24,6 +24,6 @@ public class RestAccessDeniedHandler implements AccessDeniedHandler, Serializabl
    @Override
    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException exception)
          throws IOException, ServletException {
-      SecurityUtils.sendError(response, exception, HttpServletResponse.SC_FORBIDDEN, "Access denied for the requested resources");
+      SecurityUtils.sendError(response, exception, HttpServletResponse.SC_FORBIDDEN, "Access denied for the requested resources"); // Send 403 HTTP code to the client
    }
 }
